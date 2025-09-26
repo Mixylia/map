16ba8c7411c7c2d0752399c04136e54fb950dff0
Mogento <davsmix@gmail.com>
2025-09-26 22:49:50 +0300

    0.1

---
+++ map/models/location.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  map
+//
+//  Created by давид сарангов on 26.09.2025.
+//
+
+import Foundation
+import MapKit
+struct Location {
+    let name:String
+    let cityName:String
+    let coordinate:CLLocationCoordinate2D
+    let description:String
+    let imageName:[String]
+    let link:String
+}
