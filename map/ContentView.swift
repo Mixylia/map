c824cdd6676c418c6170b397740498bafd745ac7
Mogento <davsmix@gmail.com>
2025-09-26 22:12:25 +0300

    Initial Commit

---
+++ map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  map
+//
+//  Created by давид сарангов on 26.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ map/mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapApp.swift
+//  map
+//
+//  Created by давид сарангов on 26.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
